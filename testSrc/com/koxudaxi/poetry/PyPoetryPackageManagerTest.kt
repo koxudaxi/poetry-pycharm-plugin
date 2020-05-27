ed8505207191d44c1797990c7ce17cbfe5664a7c
Koudai Aono <koxudaxi@gmail.com>
2020-05-28 01:47:53 +0900

    add testcase

--- testSrc/com/koxudaxi/poetry/PyPoetryPackageManagerTest.kt
+++ testSrc/com/koxudaxi/poetry/PyPoetryPackageManagerTest.kt
@@ -2,6 +2,9 @@ package com.koxudaxi.poetry
 
 
 import com.jetbrains.python.fixtures.PythonMockSdk
+import com.jetbrains.python.packaging.PyPackage
+import com.jetbrains.python.packaging.PyRequirement
+import com.jetbrains.python.packaging.PyRequirementParser
 
 
 class PyPoetryPackageManagerTest : PoetryTestCase() {
@@ -9,11 +12,28 @@ class PyPoetryPackageManagerTest : PoetryTestCase() {
         get() {
             return getTestDataAsText("dry-run-result.txt")
         }
+    private fun getPyPackage(name: String, version: String) :PyPackage {
+        return PyPackage(name, version, null, emptyList())
+    }
+    fun getPyRequirement(name: String, version: String): PyRequirement? {
+        return PyRequirementParser.fromLine("${name}==${version}")
+    }
     fun testParsePoetryInstallDryRun() {
         val sdk = PythonMockSdk.create("3.7")
         val pyPoetryPackageManager = PyPoetryPackageManager(sdk)
         val result = pyPoetryPackageManager.parsePoetryInstallDryRun(testDataAsText)
         assertEquals(result.first.size, 1)
+        assertEquals(result.first,
+                listOf(getPyPackage("typed-ast", "1.4.1"))
+        )
         assertEquals(result.second.size, 3)
+        assertEquals(result.second,
+                listOf(
+                        getPyRequirement("mypy-extensions","0.4.3"),
+                        getPyRequirement("typing-extensions","3.7.4.2"),
+                        getPyRequirement("mypy","0.770")
+                )
+        )
+
     }
 }
