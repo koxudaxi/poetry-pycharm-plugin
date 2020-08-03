a772950748b701726a14dfcd5ba0431a8e41a542
Koudai Aono <koxudaxi@gmail.com>
2020-08-04 04:48:06 +0900

    add toolwindow for poetry

---
+++ src/com/koxudaxi/poetry/PoetryToolWindow.kt
@@ -0,0 +1,10 @@
+package com.koxudaxi.poetry
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class PoetryToolWindow : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    }
+}
