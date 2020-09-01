58ff5c3845cc02bad5e9a3a20feb1c6286e1304f
Koudai Aono <koxudaxi@gmail.com>
2020-09-02 02:26:08 +0900

    Support RunConfiguration

--- src/com/koxudaxi/poetry/PoetryRunScript.kt
+++ src/com/koxudaxi/poetry/PoetryRunScript.kt
@@ -1,46 +1,47 @@
 package com.koxudaxi.poetry
 
+import com.intellij.execution.ExecutionManager
 import com.intellij.execution.Location
-import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.impl.ConsoleViewImpl
-import com.intellij.execution.process.*
+import com.intellij.execution.RunManager
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.actions.RunConfigurationProducer
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.wm.ToolWindowManager
-import com.jetbrains.python.packaging.PyExecutionException
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.StandardFileSystems
+import com.intellij.psi.PsiFile
+import com.jetbrains.extensions.python.toPsi
+import com.jetbrains.python.run.PythonRunConfigurationProducer
 import com.jetbrains.python.sdk.associatedModule
 import com.jetbrains.python.sdk.pythonSdk
 import org.toml.lang.psi.TomlKey
 
 
 class PoetryRunScript : AnAction() {
+    private fun runScriptFromRunConfiguration(project: Project, file: PsiFile) {
+        val configurationFromContext = RunConfigurationProducer
+                .getInstance(PythonRunConfigurationProducer::class.java)
+                .createConfigurationFromContext(ConfigurationContext(file)) ?: return
+        val settings = configurationFromContext.configurationSettings
+        val runManager = RunManager.getInstance(project)
+        runManager.addConfiguration(settings)
+        runManager.selectedConfiguration = settings
+        val builder = ExecutionEnvironmentBuilder.createOrNull(DefaultRunExecutor.getRunExecutorInstance(), settings)
+                ?: return
+        ExecutionManager.getInstance(project).restartRunProfile(builder.build())
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         val tomlKey = e.dataContext.getData(Location.DATA_KEY)?.psiElement as? TomlKey ?: return
         e.project?.pythonSdk?.associatedModule?.let {
-            val toolWindow = ToolWindowManager.getInstance(e.project!!).getToolWindow("Poetry") ?: return
-            val consoleView = ConsoleViewImpl(e.project!!, true)
-            val contentName = when (val tabCount = toolWindow.contentManager.contentCount) {
-                0 -> "Script"
-                else -> "Script(${tabCount + 1})"
-            }
-            val executable = getPoetryExecutable()?.path
-                    ?: throw PyExecutionException("Cannot find Poetry", "poetry", emptyList(), ProcessOutput())
-            val commandLine = GeneralCommandLine(executable, "run", tomlKey.text).withWorkDirectory(e.project!!.basePath)
-            val factory = ProcessHandlerFactory.getInstance()
-            val handler = factory.createColoredProcessHandler(commandLine)
-            consoleView.attachToProcess(handler)
-            ProcessTerminatedListener.attach(handler)
-            handler.setShouldDestroyProcessRecursively(true)
-            val content = toolWindow.contentManager.factory.createContent(consoleView.component, contentName, false)
-            content.setShouldDisposeContent(true)
-            content.isCloseable = true
-            toolWindow.contentManager.addContent(content)
-            toolWindow.contentManager.setSelectedContent(content)
-            toolWindow.show()
-            handler.startNotify()
+            val scriptPath = it.pythonSdk?.homeDirectory?.parent?.findChild(tomlKey.text)?.path ?: return
+            val file = StandardFileSystems.local().findFileByPath(scriptPath)?.toPsi(it.project) ?: return
+            runScriptFromRunConfiguration(it.project, file.containingFile)
         }
-
     }
+
     companion object {
         const val actionID = "poetryRunScript"
     }
